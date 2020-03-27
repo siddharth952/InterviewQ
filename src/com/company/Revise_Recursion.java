285abb185169fe84d22bd5203f418f109876863c
Siddharth <40488679+siddharth952@users.noreply.github.com>
2020-03-27 09:50:31 +0530

    DONE: A_6_Q1

--- src/com/company/Revise_Recursion.java
+++ src/com/company/Revise_Recursion.java
@@ -7,9 +7,12 @@ public class Revise_Recursion {
     public static void main(String[] args) {
         // write your code here
 
-       ArrayList<String> res =  mazePath(0,0,1,2,2);
+//       ArrayList<String> res =  mazePath(0,0,1,2,2);
+//
+//        System.out.println(res);
+//
+        ques1_A(5,5);
 
-        System.out.println(res);
     }
 
 
@@ -54,12 +57,45 @@ public class Revise_Recursion {
             //For Each String in ArrayList
             master.add("D"+rRDS);
         }
-
-
-
-
-
-
     return master;
     }
+
+    public static void eggDrop(){
+        /*
+        You are given K eggs, and you have access to a building with N floors from 1 to N.
+
+        Each egg is identical in function, and if an egg breaks, you cannot drop it again.
+
+        You know that there exists a floor F with 0 <= F <= N such that any egg dropped at a floor higher than F will break, and any egg dropped at or below floor F will not break.
+
+        Each move, you may take an egg (if you have an unbroken one) and drop it from any floor X (with 1 <= X <= N).
+
+        Your goal is to know with certainty what the value of F is.
+
+        What is the minimum number of moves that you need to know with certainty what F is, regardless of the initial value of F?
+         */
+
+
+
+    }
+
+    public static void ques1_A(int N,int end){
+        /*
+        Take as input N, a number. Print odd numbers in decreasing sequence
+        (up until 0) and even numbers in increasing sequence (up until N).
+        E.g. for N = 6 print 5, 3, 1, 2, 4.
+         */
+        if(N <= 0){
+            return;
+        }
+        System.out.print(N+",");
+        ques1_A(N-2,end);
+        if(N != end)
+        System.out.print(N+1+",");
+    }
+
+
+
+
+
 }
